321d3c3ff9030375a0dde5d89293cdddcb416f46
Victor <felipe.beleza29@gmail.com>
2020-07-10 14:30:08 -0300

    config-server na nuvem

---
+++ loja/src/main/java/br/com/example/loja/client/FornecedorClient.java
@@ -0,0 +1,13 @@
+package br.com.example.loja.client;
+
+import br.com.example.loja.controller.dto.InfoFornecedorDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("fornecedor")
+public interface FornecedorClient {
+
+    @RequestMapping("/info/{estado}")
+    InfoFornecedorDTO getInfoPorEstado(@PathVariable String estado);
+}
--- loja/src/main/java/br/com/example/loja/service/CompraService.java
+++ loja/src/main/java/br/com/example/loja/service/CompraService.java
@@ -1,24 +1,22 @@
 package br.com.example.loja.service;
 
+import br.com.example.loja.client.FornecedorClient;
 import br.com.example.loja.controller.dto.CompraDTO;
 import br.com.example.loja.controller.dto.InfoFornecedorDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class CompraService {
 
     @Autowired
-    private RestTemplate client;
+    private FornecedorClient fornecedorClient;
 
     public void realizaCompra(CompraDTO compra) {
 
-        ResponseEntity<InfoFornecedorDTO> exchange = client.exchange("http://fornecedor/info/" + compra.getEndereco().getEstado(),
-                HttpMethod.GET, null, InfoFornecedorDTO.class);
+        InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
 
-        System.out.println(exchange.getBody().getEndereco());
+        System.out.println(info.getEndereco());
     }
+
 }
